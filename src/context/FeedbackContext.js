82fb15d3682bdbeb665b66a5587c802db712edd4
rinorselmani9 <rs44583@ubt-uni.net>
2022-01-12 13:12:56 +0100

    first commit on the app

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,67 @@
+import {createContext, useState} from 'react'
+import { v4 as uuidv4 } from "uuid"
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({children}) => {
+
+    const  [feedback,setFeedback] = useState([
+        {
+            id:1,
+            text:'This is feedback item 1',
+            rating:6
+        },
+        {
+            id:2,
+            text:'This is feedback item 2',
+            rating:9
+        },
+        {
+            id:3,
+            text:'This is feedback item 3',
+            rating:10
+        }
+
+    ])
+
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item : {},
+        edit : false
+    })
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
+    const deleteFeedback = (id) => {
+        if(window.confirm("Are you sure you wanna delete?")){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
+    const updateFeedback = (id, updItem) => {
+            setFeedbackEdit(
+                feedback.map((item)=> (item.id === id ? { ...item, ...updItem } : item))
+                )
+    }
+
+    const editFeedback = (item) =>{
+        setFeedbackEdit ({
+            item,
+            edit:true
+        })
+    }
+
+    return <FeedbackContext.Provider value={{
+        feedback,
+        feedbackEdit,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback
+    }} >
+        {children}
+    </FeedbackContext.Provider>
+}
+export default FeedbackContext
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import Card from "../components/shared/Card"
+
+
+function AboutPage() {
+    return (
+        <Card>
+            <div className="about">
+                <h1>About this project</h1>
+                <p>This is a React App to leave a feedback for a product or service</p>
+                <p>Version 1.0.0</p>
+                <Link to='/'>Back To Home</Link>
+            </div>
+            
+        </Card>
+    )
+}
+
+export default AboutPage
